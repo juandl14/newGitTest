8042b8c99666817c0d2fb9eee9d814936c109f4e
Juan DL <jdeluca@itba.edu.ar>
2020-06-29 19:51:20 -0300

    agregado var2 y constructor

--- src/poo/taller/Main.java
+++ src/poo/taller/Main.java
@@ -1,4 +1,12 @@
 package poo.taller;
 
 public class Main {
+
+    private int var1;
+    private int var2;
+
+    public Main(int var1, int var2) {
+        this.var1 = var1;
+        this.var2 = var2;
+    }
 }
